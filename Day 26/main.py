882d0a213b36d04496cfc57424987276119c03d9
Suroj Rana <surojrana2014@gmail.com>
2025-11-20 14:22:32 +0545

    Update the Project

---
+++ Day 26/main.py
@@ -0,0 +1,39 @@
+numbers=[2,4,6,8]
+# new_list=[]
+# for n in numbers:
+#     add_2=n+2
+#     new_list.append(add_2)
+
+# print(new_list)
+
+#LIST COMPREHENSION
+# new_list1=[n + 2 for n in numbers]
+# print(new_list1)
+
+# name="Angela"
+# letter_list=[letter for letter in name ]
+# print(letter_list)
+
+# list=range(1,5)
+# new_list3=[n*2 for n in list ]
+# print(new_list3)
+#
+# # SHORTCUT
+#
+# new_list4=[n *2 for n in range(1,5)]
+# print(new_list4)
+
+#CONDITIONAL LIST COMPREHENSION
+#
+# names=["suroj","saroj","Cena","dhiroj","niroj","John","Hari"]
+# short_names=[name.upper() for name in names if len(name) >5]
+# print(short_names)
+#
+# list_of_strings = ['9', '0', '32', '8', '2', '8', '64', '29', '42', '99']
+# numbers = [int(n) for n in list_of_strings]
+# result=[num for num in numbers if num%2==0]
+# print(result)
+
+#DICTIONARY COMPREHENSION
+
+
