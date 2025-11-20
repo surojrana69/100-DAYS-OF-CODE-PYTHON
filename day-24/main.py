a318d686bdded6248b6c59959f711ab6bfd9baad
Suroj Rana <surojrana2014@gmail.com>
2025-11-20 14:21:35 +0545

    Day 24 Project updated

---
+++ day-24/main.py
@@ -0,0 +1,10 @@
+#READING THE FILE
+with open(r"C:\Users\suroj\OneDrive\Desktop\suroj.txt") as file:
+    contents=file.read()
+    print(contents)
+
+#WRITING THE FILE
+
+# with open("rana.txt",mode="a") as file:
+#     file.write("I am fine ")
+
